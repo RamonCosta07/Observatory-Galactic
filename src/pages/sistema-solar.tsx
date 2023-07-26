38dc5e908afd2baf538839e183411d8f82f7f207
RamonCosta07 <ramonnar20@hotmail.com>
2023-07-26 19:15:29 -0300

    Adicionado setTimeout no sistema solar

--- src/pages/sistema-solar.tsx
+++ src/pages/sistema-solar.tsx
@@ -4,18 +4,32 @@ import { Button } from "@/styles/ButtonStyles";
 import * as S from "@/styles/SistemaSolarStyles";
 // Components
 import Planets from "@/components/Planets";
+import Loading from "@/components/Loading";
 // Next
 import Head from "next/head";
 import { useRouter } from "next/router";
 // React Icons
 import { AiOutlineArrowRight } from "react-icons/ai";
+// Hooks
+import { useState } from "react";
 
 const SistemaSolar = () => {
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 4000);
 
   const handleNextPage = () => {
     router.push("/sistema-solar/2");
   };
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
 
   return (
     <>
