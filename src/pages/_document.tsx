411fc50f37ccd18cc127f2c87a6efed4b2a37f2f
RamonCosta07 <ramonnar20@hotmail.com>
2023-07-28 19:53:41 -0300

    Bloqueada função de rotacionar dispositivo para manter layout consistente

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
